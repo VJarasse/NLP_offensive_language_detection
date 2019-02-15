88f745793e235eb55d853efa07b446d2f035ae5d
VJarasse <vincent.jarasse@gmail.com>
2019-02-15 15:52:22 +0000

    Improvements on corpus management

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import numpy as np
 from corpus import *
 from neuralnet import *
 from utils import *
-import os
 
 # Loading the dataset
 dataframe = pd.read_csv('data/offenseval-training-v1.tsv', sep="\t", header=0)
 
-
 # Use once to save the clean vocabulary to a file
-
+'''
 corpus = Corpus(dataframe, 'tweet')
 with open("vocabulary.txt", "w") as f:
     for voc in corpus.clean_vocabulary:
@@ -20,11 +18,15 @@ with open("corpus_tweets.txt", "w") as f:
         for token in sentence:
             f.write(token + " ")
         f.write("\n")
-
+'''
 # Get the vocabulary back
-vocabulary = []
-with open("vocabulary.txt", "r") as file:
-        vocabulary = file.read().splitlines()
+with open("data/vocabulary.txt", "r") as file:
+    vocabulary = file.read().splitlines()
+# Get the cleaned tokenized corpus back
+with open("data/corpus_tweets.txt", "r") as file:
+    tmp = file.read().splitlines()
+# In each sentence, we get rid of the last token, which is '\n'
+tokenized_corpus = [[token for token in sentence.split(' ')][:-1] for sentence in tmp]
 
 print("Length of vocabulary is : ", len(vocabulary), " words and expressions")
 
@@ -34,44 +36,16 @@ epochs = 20
 # the input dimension is the vocabulary size
 INPUT_DIM = len(vocabulary)
 # we define our embedding dimension (dimensionality of the output of the first layer)
-EMBEDDING_DIM = 100
+EMBEDDING_DIM = 10000
 # dimensionality of the output of the second hidden layer
-HIDDEN_DIM = 50
+HIDDEN_DIM = 500
 # the output dimension is the number of classes, 1 for binary classification
 OUTPUT_DIM = 1
 #
 sent_lengths = [len(sent) for sent in tokenized_corpus]
-
-word2idx = get_word2idx(vocabulary)
-
-
-
-
-
-
-def get_model_inputs(tokenized_corpus, word2idx, labels, max_len):
-        # we index our sentences
-        vectorized_sents = [[word2idx[tok] for tok in sent if tok in word2idx] for sent in tokenized_corpus]
-
-        # we create a tensor of a fixed size filled with zeroes for padding
-
-        sent_tensor = Variable(torch.zeros((len(vectorized_sents), max_len))).long()
-
-        sent_lengths = [len(sent) for sent in vectorized_sents]
-
-        # we fill it with our vectorized sentences
-
-        for idx, (sent, sentlen) in enumerate(zip(vectorized_sents, sent_lengths)):
-                sent_tensor[idx, :sentlen] = torch.LongTensor(sent)
-
-        label_tensor = torch.FloatTensor(labels)
-
-        return sent_tensor, label_tensor
-
-
-
 max_len = np.max(np.array(sent_lengths))
 
+word2idx = get_word2idx(vocabulary)
 
 train_sent_tensor, train_label_tensor = get_model_inputs(tokenized_corpus, word2idx, train_labels, max_len)
 
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import torch
+from tqdm import tqdm
+import codecs
+import random
 
 
 def accuracy(output, target):
@@ -13,3 +16,33 @@ def get_word2idx(vocabulary):
     # we reserve the 0 index for the placeholder token
     word2idx['<pad>'] = 0
     return word2idx
+
+
+def tokenize(corpus, sep=' '):
+    tokenized_corpus = []
+    for sentence in corpus:
+        tokenized_sentence = []
+        for token in sentence.split(sep):
+            tokenized_sentence += token
+        tokenized_corpus.append(tokenized_sentence)
+    return tokenized_corpus
+
+
+def get_model_inputs(tokenized_corpus, word2idx, labels, max_len):
+    # we index our sentences
+    vectorized_sents = [[word2idx[tok] for tok in sent if tok in word2idx] for sent in tokenized_corpus]
+
+    # we create a tensor of a fixed size filled with zeroes for padding
+
+    sent_tensor = torch.zeros((len(vectorized_sents), max_len)).long()
+
+    sent_lengths = [len(sent) for sent in vectorized_sents]
+
+    # we fill it with our vectorized sentences
+
+    for idx, (sent, sentlen) in enumerate(zip(vectorized_sents, sent_lengths)):
+        sent_tensor[idx, :sentlen] = torch.LongTensor(sent)
+
+    label_tensor = torch.FloatTensor(labels)
+
+    return sent_tensor, label_tensor
