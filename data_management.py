c9b0d2e1ded1ae7aefe7f42e521d03a6c67275c3
VJarasse <vincent.jarasse@gmail.com>
2019-03-01 23:41:25 +0000

    Added jupyter notebook file

--- data_management.py
+++ data_management.py
@@ -76,8 +76,87 @@ def embedding():
     return ratio
 
 
+def corpus_and_voc_test(path, task):
+    dataframe = pd.read_csv(path, sep="\t", header=0)
+    id = dataframe['id']
+
+
+    # Use once to save the clean vocabulary
+    corpus = Corpus(dataframe, 'tweet')
+    with open("data/test_vocabulary_" + task + ".txt", "w") as f:
+        for voc in corpus.clean_vocabulary:
+            f.write(voc + "\n")
+
+    with open("data/test_corpus_tweets_" + task + ".txt", "w") as f:
+        for sentence in corpus.tokenized_corpus:
+            for token in sentence:
+                f.write(token + " ")
+            f.write("\n")
+
+
+def embedding_test(task):
+    # Get back clean vocabulary from txt file
+    with open("data/test_vocabulary_" + task + ".txt", "r") as file:
+        vocabulary = file.read().splitlines()
+
+    # Keep track of unknown words for later processing
+    found = 0.
+    voc_size = len(vocabulary)
+
+    with open("data/emb_dic_" + task + ".txt", "w") as f, open("data/glove.twitter.27B.100d.txt", "r") as glove:
+        # Create dictionary of all glove embedding from txt file
+        emb_dict = {}
+        rejected = []
+        for line in glove:
+            values = line.split()
+            word = values[0]
+            if word in vocabulary:
+                found += 1
+                try:
+                    vector = np.asarray(values[1:], dtype='float32')
+                    f.write(line)
+                except:
+                    print("Parsing problem on word ", word, " discarding it")
+        glove.close()
+    ratio = found / voc_size * 100
+    return ratio
+
+
+# task is of form "subtask_a"
+def get_subset(task):
+    dataframe = pd.read_csv('data/offenseval-training-v1.tsv', sep="\t", header=0)
+    sub_dataframe = dataframe[dataframe[task].notnull()]
+    return sub_dataframe
+
+
+def corpus_subset(task):
+    dataframe = get_subset(task)
+    label1 = dataframe[task]
+
+    # Use once to save the clean vocabulary
+    corpus = Corpus(dataframe, 'tweet')
+
+    with open("data/corpus_tweets_" + task + ".txt", "w") as f:
+        for sentence in corpus.tokenized_corpus:
+            for token in sentence:
+                f.write(token + " ")
+            f.write("\n")
+
+    with open("data/labels_" + task + ".txt", "w") as f:
+        for label in label1:
+            if label == "IND":
+                f.write("1\n")
+            elif label == "GRP":
+                f.write("2\n")
+            elif label == "OTH":
+                f.write("0\n")
+
+#corpus_subset("subtask_c")
+
 #corpus_and_voc()
 
-ratio = embedding()
-print("Ratio of found words ", ratio)
+#ratio = embedding()
+#print("Ratio of found words ", ratio)
 
+#corpus_and_voc_test("data/test_set_taskc.tsv", "c")
+embedding_test("c")
